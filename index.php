b73bd9b603cb379a238aa5a31484fdfbb49312ac
87 <144358868+BrayneL87@users.noreply.github.com>
2023-10-09 15:00:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 
 
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-<h1>
     <?php echo "Hola"; ?>
-</h1>
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
-
-</body>
-</html>
